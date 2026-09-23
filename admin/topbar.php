315001d16c6d307cd0b11bfd541a19c93d8e0b73
thegunjanthakkar <117365813+thegunjanthakkar@users.noreply.github.com>
2026-09-23 16:37:30 +0530

    updated topbar

--- admin/topbar.php
+++ admin/topbar.php
@@ -233,13 +233,13 @@ $type_colors = [
                                 </div><!--end media-body-->
                             </div>
                             <div class="dropdown-divider mt-0"></div>
-                            <small class="text-muted px-2 pb-1 d-block">Account</small>
+                            <!-- <small class="text-muted px-2 pb-1 d-block">Account</small>
                             <a class="dropdown-item" href="#"><i class="las la-user fs-18 me-1 align-text-bottom"></i> Profile</a>
                             <a class="dropdown-item" href="#"><i class="las la-wallet fs-18 me-1 align-text-bottom"></i> Earning</a>
                             <small class="text-muted px-2 py-1 d-block">Settings</small>                        
                             <a class="dropdown-item" href="#"><i class="las la-cog fs-18 me-1 align-text-bottom"></i>Account Settings</a>
                             <a class="dropdown-item" href="#"><i class="las la-lock fs-18 me-1 align-text-bottom"></i> Security</a>
-                            <a class="dropdown-item" href="#"><i class="las la-question-circle fs-18 me-1 align-text-bottom"></i> Help Center</a>                       
+                            <a class="dropdown-item" href="#"><i class="las la-question-circle fs-18 me-1 align-text-bottom"></i> Help Center</a>                        -->
                             <div class="dropdown-divider mb-0"></div>
                             <a class="dropdown-item text-danger" href="#" onclick="confirmLogout(); return false;"><i class="las la-power-off fs-18 me-1 align-text-bottom"></i> Logout</a>
                         </div>
